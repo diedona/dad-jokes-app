bacc3ea34a96bc2a58e3ad917e65287dc9787cc7
Diego Doná <diedona@gmail.com>
2020-09-27 18:27:42 -0300

    Updates

---
+++ precache-manifest.9456e1b66141fc3aa34a79828000231b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e465b0f005f593882e2108f7abfc3321",
+    "url": "/dad-jokes-app/index.html"
+  },
+  {
+    "revision": "93bb8d5cf678297d7573",
+    "url": "/dad-jokes-app/static/css/main.a4676005.chunk.css"
+  },
+  {
+    "revision": "76057c515d765a21d5b7",
+    "url": "/dad-jokes-app/static/js/2.33a592bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes-app/static/js/2.33a592bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93bb8d5cf678297d7573",
+    "url": "/dad-jokes-app/static/js/main.5b0dbea6.chunk.js"
+  },
+  {
+    "revision": "9856f8952f36199beb0d",
+    "url": "/dad-jokes-app/static/js/runtime-main.f279d699.js"
+  }
+]);
